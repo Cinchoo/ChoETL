36681487e5c527080cdff63dc5a05609d5bcdcc0
Cinchoo <cinchoofrx@gmail.com>
2017-01-24 14:42:40 -0500

    FIXES

---
+++ src/ChoETL/File/FixedLength/ChoFixedLengthWriter.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChoETL
+{
+    public class ChoFixedLengthWriter<T> : IDisposable
+        where T : class
+    {
+        private StreamWriter _streamWriter;
+        private bool _closeStreamOnDispose = false;
+        private ChoFixedLengthRecordWriter _writer = null;
+        private bool _clearFields = false;
+
+        public ChoFixedLengthRecordConfiguration Configuration
+        {
+            get;
+            private set;
+        }
+
+        public ChoFixedLengthWriter(string filePath, ChoFixedLengthRecordConfiguration configuration = null)
+        {
+            ChoGuard.ArgumentNotNullOrEmpty(filePath, "FilePath");
+
+            Configuration = configuration;
+
+            Init();
+
+            _streamWriter = new StreamWriter(ChoPath.GetFullPath(filePath), false, Configuration.Encoding, Configuration.BufferSize);
+            _closeStreamOnDispose = true;
+        }
+
+        public ChoFixedLengthWriter(StreamWriter streamWriter, ChoFixedLengthRecordConfiguration configuration = null)
+        {
+            ChoGuard.ArgumentNotNull(streamWriter, "StreamWriter");
+
+            Configuration = configuration;
+            Init();
+
+            _streamWriter = streamWriter;
+        }
+
+        public ChoFixedLengthWriter(Stream inStream, ChoFixedLengthRecordConfiguration configuration = null)
+        {
+            ChoGuard.ArgumentNotNull(inStream, "Stream");
+
+            Configuration = configuration;
+            Init();
+            _streamWriter = new StreamWriter(inStream, Configuration.Encoding, Configuration.BufferSize);
+            _closeStreamOnDispose = true;
+        }
+
+        public void Dispose()
+        {
+            if (_closeStreamOnDispose)
+                _streamWriter.Dispose();
+        }
+
+        private void Init()
+        {
+            if (Configuration == null)
+                Configuration = new ChoFixedLengthRecordConfiguration(typeof(T));
+
+            _writer = new ChoFixedLengthRecordWriter(typeof(T), Configuration);
+        }
+
+        public void Write(IEnumerable<T> records)
+        {
+            _writer.WriteTo(_streamWriter, records).Loop();
+        }
+
+        public void Write(T record)
+        {
+            _writer.WriteTo(_streamWriter, new T[] { record } ).Loop();
+        }
+
+        public static string ToText<TRec>(IEnumerable<TRec> records)
+            where TRec : class
+        {
+            using (var stream = new MemoryStream())
+            using (var reader = new StreamReader(stream))
+            using (var writer = new StreamWriter(stream))
+            using (var parser = new ChoFixedLengthWriter<TRec>(writer))
+            {
+                parser.Write(records);
+
+                writer.Flush();
+                stream.Position = 0;
+
+                return reader.ReadToEnd();
+            }
+        }
+
+        #region Fluent API
+
+        public ChoFixedLengthWriter<T> WithRecordLength(int length)
+        {
+            Configuration.RecordLength = length;
+            return this;
+        }
+
+        public ChoFixedLengthWriter<T> WithFirstLineHeader(bool flag = true)
+        {
+            Configuration.FileHeaderConfiguration.HasHeaderRecord = flag;
+            return this;
+        }
+
+        public ChoFixedLengthWriter<T> QuoteAllFields(bool flag = true, char quoteChar = '"')
+        {
+            Configuration.QuoteAllFields = flag;
+            Configuration.QuoteChar = quoteChar;
+            return this;
+        }
+
+        public ChoFixedLengthWriter<T> WithField(string fieldName, int startIndex, int size, Type fieldType, bool? quoteField = null)
+        {
+            if (!fieldName.IsNullOrEmpty())
+            {
+                if (!_clearFields)
+                {
+                    Configuration.RecordFieldConfigurations.Clear();
+                    _clearFields = true;
+                }
+                if (fieldType == null)
+                    fieldType = typeof(string);
+
+                Configuration.RecordFieldConfigurations.Add(new ChoFixedLengthRecordFieldConfiguration(fieldName.Trim(), startIndex, size) { FieldType = fieldType, QuoteField = quoteField });
+            }
+
+            return this;
+        }
+
+        public ChoFixedLengthWriter<T> WithField(string fieldName, int startIndex, int size, bool? quoteField = null)
+        {
+            return WithField(fieldName, startIndex, size, typeof(string), quoteField);
+        }
+
+        #endregion Fluent API
+    }
+
+    public class ChoFixedLengthWriter : ChoFixedLengthWriter<ExpandoObject>
+    {
+        public ChoFixedLengthWriter(string filePath, ChoFixedLengthRecordConfiguration configuration = null)
+            : base(filePath, configuration)
+        {
+
+        }
+        public ChoFixedLengthWriter(StreamWriter streamWriter, ChoFixedLengthRecordConfiguration configuration = null)
+            : base(streamWriter, configuration)
+        {
+        }
+
+        public ChoFixedLengthWriter(Stream inStream, ChoFixedLengthRecordConfiguration configuration = null)
+            : base(inStream, configuration)
+        {
+        }
+
+        public void Write(IDataReader dr)
+        {
+            ChoGuard.ArgumentNotNull(dr, "DataReader");
+
+            DataTable schemaTable = dr.GetSchemaTable();
+            var expando = new ExpandoObject();
+            var expandoDic = (IDictionary<string, object>)expando;
+
+            //int ordinal = 0;
+            if (Configuration.RecordFieldConfigurations.IsNullOrEmpty())
+            {
+                string colName = null;
+                Type colType = null;
+                foreach (DataRow row in schemaTable.Rows)
+                {
+                    colName = row["ColumnName"].CastTo<string>();
+                    colType = row["DataType"] as Type;
+                    //if (!colType.IsSimple()) continue;
+
+                    //Configuration.RecordFieldConfigurations.Add(new ChoFixedLengthRecordFieldConfiguration(colName, ++ordinal) { FieldType = colType });
+                }
+            }
+
+            while (dr.Read())
+            {
+                expandoDic.Clear();
+
+                foreach (var fc in Configuration.RecordFieldConfigurations)
+                {
+                    expandoDic.Add(fc.Name, dr[fc.Name]);
+                }
+
+                Write(expando);
+            }
+        }
+
+        public void Write(DataTable dt)
+        {
+            ChoGuard.ArgumentNotNull(dt, "DataTable");
+
+            DataTable schemaTable = dt;
+            var expando = new ExpandoObject();
+            var expandoDic = (IDictionary<string, object>)expando;
+
+            int ordinal = 0;
+            if (Configuration.RecordFieldConfigurations.IsNullOrEmpty())
+            {
+                string colName = null;
+                Type colType = null;
+                //foreach (DataColumn col in schemaTable.Columns)
+                //{
+                //    colName = col.ColumnName;
+                //    colType = col.DataType;
+                //    //if (!colType.IsSimple()) continue;
+
+                //    Configuration.RecordFieldConfigurations.Add(new ChoFixedLengthRecordFieldConfiguration(colName, ++ordinal) { FieldType = colType });
+                //}
+            }
+
+            foreach (DataRow row in dt.Rows)
+            {
+                expandoDic.Clear();
+
+                foreach (var fc in Configuration.RecordFieldConfigurations)
+                {
+                    expandoDic.Add(fc.Name, row[fc.Name]);
+                }
+
+                Write(expando);
+            }
+        }
+    }
+}
