84dea7ca4052c011b9f34be3265fb93f1532fb9e
Cinchoo <cinchoofrx@gmail.com>
2017-01-26 10:08:33 -0500

    FIX

--- src/Test/ChoFixedLengthWriterTest/Program.cs
+++ src/Test/ChoFixedLengthWriterTest/Program.cs
@@ -33,16 +33,17 @@ namespace ChoFixedLengthWriterTest
             dynamic rec2 = new ExpandoObject();
             rec2.Id = 2000;
             rec2.Name = "Lou";
-            rec2.JoinedDate = new DateTime(1990, 10, 23);
+            rec2.JoinedDate = null; // new DateTime(1990, 10, 23);
             rec2.IsActive = false;
-            rec2.Salary = new ChoCurrency(150000);
+            rec2.Salary = null; // new ChoCurrency(150000);
             objs.Add(rec2);
 
             using (var stream = new MemoryStream())
             using (var reader = new StreamReader(stream))
             using (var writer = new StreamWriter(stream))
             using (var parser = new ChoFixedLengthWriter(writer).WithFirstLineHeader().WithField("Id", 0, 3, null, '0', ChoFieldValueJustification.Right, true).WithField("Name", 3, 10).
-                WithField("JoinedDate", 13, 10).WithField("IsActive", 23, 1, fieldName: "A"))
+                WithField("JoinedDate", 13, 10, fieldType: typeof(DateTime), fillChar: '0').WithField("IsActive", 23, 1, fieldName: "A").
+                WithField("Salary", 24, 20))
             {
                 parser.Configuration["Name"].AddConverter(new ChoUpperCaseConverter());
                 parser.Write(objs);
