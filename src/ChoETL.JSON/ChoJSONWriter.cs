e5540d020ffe2b71239fd038b91fd31ec3640bf4
Cinchoo <Cinchoofrx@gmail.com>
2018-07-17 21:48:55 -0400

    Fixes

--- src/ChoETL.JSON/ChoJSONWriter.cs
+++ src/ChoETL.JSON/ChoJSONWriter.cs
@@ -146,16 +146,6 @@ namespace ChoETL
                 _writer.WriteTo(_textWriter, new T[] { record }).Loop();
         }
 
-        public static string SerializeAll(IEnumerable<T> records, ChoJSONRecordConfiguration configuration = null, TraceSwitch traceSwitch = null)
-        {
-            return ToTextAll<T>(records, configuration, traceSwitch);
-        }
-
-        public static string Serialize(T record, ChoJSONRecordConfiguration configuration = null, TraceSwitch traceSwitch = null)
-        {
-            return ToText<T>(record, configuration, traceSwitch);
-        }
-
         public static string ToText<TRec>(TRec record, ChoJSONRecordConfiguration configuration = null, TraceSwitch traceSwitch = null, string jsonPath = null)
             where TRec : class
         {
@@ -539,6 +529,28 @@ namespace ChoETL
         public ChoJSONWriter(Stream inStream, ChoJSONRecordConfiguration configuration = null)
             : base(inStream, configuration)
         {
+        }
+
+        public static string SerializeAll(IEnumerable<dynamic> records, ChoJSONRecordConfiguration configuration = null, TraceSwitch traceSwitch = null)
+        {
+            return ToTextAll(records, configuration, traceSwitch);
+        }
+
+        public static string SerializeAll<T>(IEnumerable<T> records, ChoJSONRecordConfiguration configuration = null, TraceSwitch traceSwitch = null)
+            where T : class
+        {
+            return ToTextAll<T>(records, configuration, traceSwitch);
+        }
+
+        public static string Serialize(dynamic record, ChoJSONRecordConfiguration configuration = null, TraceSwitch traceSwitch = null)
+        {
+            return ToText(record, configuration, traceSwitch);
+        }
+
+        public static string Serialize<T>(T record, ChoJSONRecordConfiguration configuration = null, TraceSwitch traceSwitch = null)
+            where T : class
+        {
+            return ToText<T>(record, configuration, traceSwitch);
         }
     }
 }
