0ad7e44c28c8a36343b559a8ff621fdb9e8d44d3
Cinchoo <Cinchoofrx@gmail.com>
2021-09-20 07:53:43 -0400

    ChoDateTime.TryParseExact is redundant and noisy #154

--- src/ChoETL/Common/ExtensionMethods/ChoDateTime.cs
+++ src/ChoETL/Common/ExtensionMethods/ChoDateTime.cs
@@ -10,16 +10,22 @@ namespace ChoETL
     {
         public static bool TryParseExact(this string value, string format, IFormatProvider formatProvider, out DateTime result)
         {
-            result = DateTime.MinValue;
-            try
-            {
-                result = DateTime.ParseExact(value, format, formatProvider);
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
+            return TryParseExact(value, format, formatProvider, System.Globalization.DateTimeStyles.None, out result);
+
+            //result = DateTime.MinValue;
+            //try
+            //{
+            //    result = DateTime.ParseExact(value, format, formatProvider);
+            //    return true;
+            //}
+            //catch
+            //{
+            //    return false;
+            //}
+        }
+        public static bool TryParseExact(this string value, string format, IFormatProvider formatProvider, System.Globalization.DateTimeStyles dateTimeStyles, out DateTime result)
+        {
+            return DateTime.TryParseExact(value, format, formatProvider, dateTimeStyles, out result);
         }
     }
 }
